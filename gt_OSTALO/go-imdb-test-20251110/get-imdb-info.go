875dbf5dc20f827d3bc7afa2793a5e5ab0e024a4
r-gregor <gredelonghi@gmail.com>
2025-11-10 14:26:00 +0100

    update 20251110_1426_en

---
+++ gt_OSTALO/go-imdb-test-20251110/get-imdb-info.go
@@ -0,0 +1,37 @@
+// filename: get-imdb-info.go
+// from: https://github.com/sraz001/goIMDB
+
+package main
+
+import (
+	"fmt"
+	"github.com/sraz001/goIMDB"
+	"log"
+)
+
+func main() {
+	m := goIMDB.InitIMDBPie()
+	// search for The Matrix title:
+	result, e := m.SearchTitle("The Matrix", 1999)
+	if e != nil {
+		log.Fatal(e)
+	}
+	// print the first result
+	fmt.Printf("Got %d Search Results, the first one is %s - %s (%d)\n", len(result), result[0].ID, result[0].Title, result[0].Year)
+	// Got 8 Search Results, the first one is tt0133093 - The Matrix (1999)
+
+	// get Basic title information
+	title, e := m.GetTitle(result[0].ID)
+	if e != nil {
+		log.Fatal(e)
+	}
+	fmt.Printf("Plot of %s is [ %s ]\n", title.ID, title.PlotOutline)
+	// Plot of tt0133093 is [ When a beautiful stranger leads computer hacker Neo to a forbidding underworld, he discovers the shocking truth--the life he knows is the elaborate deception of an evil cyber-intelligence. ]
+
+	// get the cast of the title
+	credits, _ := title.GetTitleCredits() // skip error check for readability
+	actor := credits["cast"][0]           // get first actor
+	fmt.Printf("Actor %s is %s with Image url: %s", actor.ID, actor.Name, actor.Image.Url)
+	// Actor nm0000206 is Keanu Reeves with Image url: https://m.media-amazon.com/images/M/MV5BNGJmMWEzOGQtMWZkNS00MGNiLTk5NGEtYzg1YzAyZTgzZTZmXkEyXkFqcGdeQXVyMTE1MTYxNDAw._V1_.jpg
+
+}
---
+++ gt_OSTALO/go-imdb-test-20251110/go.mod
@@ -0,0 +1,11 @@
+module get-imdb-info.go
+
+go 1.23.0
+
+require github.com/sraz001/goIMDB v0.0.0-20230103142809-e122b046d2cc
+
+require (
+	github.com/aws/aws-sdk-go v1.44.171 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
